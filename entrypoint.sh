01138c6e74f53c2d4ffba0ee3ffc7253cea3484f
Alex Hopgood <alex.hopgood@gmail.com>
2026-04-05 16:14:04 +0100

    Adding draft support to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,15 @@
 set -ex
 
 FUTURE_FLAG=""
-if [ -n "${FUTURE}"  ] && [ "${FUTURE}" == "true" ] ; then
+if [ -n "${FUTURE}"  ] && [ "${FUTURE}" == "true" ]; then
   echo "Future env var found, setting flag --future"
   FUTURE_FLAG="--future"
 fi
 
-bundle exec jekyll serve -s /srv/jekyll --host 0.0.0.0 ${FUTURE_FLAG}
+DRAFT_FLAG=""
+if [ -n "${DRAFT}" ] && [ "${DRAFT}" == "true" ]; then
+  echo "Draft env var found, setting flag --draft"
+  DRAFT_FLAG="--draft"
+fi
+
+bundle exec jekyll serve -s /srv/jekyll --host 0.0.0.0 ${FUTURE_FLAG} ${DRAFT_FLAG}
