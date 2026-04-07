53fd2fdf7f49e6eabf8a0ac5fa5331f4332e0f9a
Alex Hopgood <alex.hopgood@gmail.com>
2026-04-07 19:40:28 +0100

    Adding watch and force polling support to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,4 +14,16 @@ if [ -n "${DRAFT}" ] && [ "${DRAFT}" == "true" ]; then
   DRAFT_FLAG="--draft"
 fi
 
-bundle exec jekyll serve -s /srv/jekyll --host 0.0.0.0 ${FUTURE_FLAG} ${DRAFT_FLAG}
+WATCH_FLAG=""
+if [ -n "${WATCH}" ] && [ "${WATCH}" == "true" ]; then
+  echo "Watch env var found, setting flag --watch"
+  WATCH_FLAG="--watch"
+fi
+
+FORCE_POLLING_FLAG=""
+if [ -n "${FORCE_POLLING}" ] && [ "${FORCE_POLLING}" == "true" ]; then
+  echo "Force polling env var found, setting flag --force-polling"
+  FORCE_POLLING_FLAG="--force-polling"
+fi
+
+bundle exec jekyll serve -s /srv/jekyll --host 0.0.0.0 ${FUTURE_FLAG} ${DRAFT_FLAG} ${WATCH_FLAG} ${FORCE_POLLING_FLAG}
